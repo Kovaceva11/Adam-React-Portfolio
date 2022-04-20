f4c6bb94b983bfcd815863ea32e7d2d4035db2bf
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2022-04-20 00:22:07 -0400

    ui

--- adams-react-portfolio/src/components/portfolio.js
+++ adams-react-portfolio/src/components/portfolio.js
@@ -714,7 +714,7 @@ export default function Portfolio() {
       </div>
       <div className="bgimg-3 ak-display-container ak-opacity-min">
         <div className="ak-display-middle" style={{whiteSpace:'nowrap'}}>
-          <span className="ak-xxlarge ak-padding-large ak-black ak-text-white ak-wide">
+          <span className="ak-proHistory ak-padding-large ak-black ak-text-white ak-wide">
             Professional History
           </span>   
           
